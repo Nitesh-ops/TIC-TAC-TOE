949406db6c67779b0f92b2ef45c14d5c80f8163a
Nitesh-ops <78256589+Nitesh-ops@users.noreply.github.com>
2021-11-10 19:15:06 +0530

    Add files via upload

---
+++ Upper.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Upper=()=>{
+    return(
+        <div class="wrapper">
+        <div className="typing-demo">
+        <h1 className="text-info text-uppercase">Welcome to TIC-TAC-TOE Game</h1>
+        </div>
+        </div>
+    );
+};
+
+export default Upper
